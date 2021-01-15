aab4d15abc9f151b96f8e6feba02491f71d3a459
patdpat <s.kietchai@outlook.com>
2021-01-15 23:30:56 +0700

    add enquiry model

--- index/models.py
+++ index/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+
+class Enquiry(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    message = models.CharField(max_length=200)
+    add_date = models.DateTimeField('date added')
+    def __str__(self):
+        return self.name
