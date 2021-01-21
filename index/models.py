95037a0bad462baaad33ff230e9a469c766db681
patdpat <s.kietchai@outlook.com>
2021-01-22 02:30:56 +0700

    add view function

--- index/models.py
+++ index/models.py
@@ -9,6 +9,5 @@ class Enquiry(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     message = models.CharField(max_length=200)
-    add_date = models.DateTimeField('date added')
     def __str__(self):
         return self.name
--- index/urls.py
+++ index/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_enquiry/', views.add_enquiry, name='add_enquiry'),
 ]
